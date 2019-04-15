a75fcf0804a8b797f0616705c323667e212f2ab1
Alexander Bukharin <alexawm182@gmail.com>
2019-04-15 23:31:57 +0300

    add procfile

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 import utils
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def get_json_data():
     return utils.get_valutes()
+
+
+if __name__ == '__main__':
+    app.run()
