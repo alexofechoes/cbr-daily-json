2e6a391b9485e2c647b9c5f31b26948c5a8cd15c
Alexander Bukharin <alexawm182@gmail.com>
2019-05-14 19:17:07 +0300

    add cache

--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify
+from flask_caching import Cache
 from .cbr import CBRFetcher
 
 
 app = Flask(__name__)
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache.init_app(app)
 cbr_fetcher = CBRFetcher()
+CACHE_TIME = 60 * 5
 
 
 @app.route("/")
+@cache.cached(timeout=CACHE_TIME)
 def get_json_data():
     currency_rates = cbr_fetcher.currency_rates()
     return jsonify(currency_rates)
